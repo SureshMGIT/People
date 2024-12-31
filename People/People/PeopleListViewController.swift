6ddcc3b19b0c3549d74ebb4c559279c20c00ff84
Suresh M <sureshkbm90@gmail.com>
2024-12-31 13:06:58 +0530

    Initial setup

---
+++ People/People/PeopleListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PeopleListViewController.swift
+//  People
+//
+//  Created by Suresh M on 31/12/24.
+//
+
+import UIKit
+
+final class PeopleListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
